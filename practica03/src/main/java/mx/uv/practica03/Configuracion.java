1c13652ea052b67a7c1b8b9cc0de25399a34f5e1
Breayanxd <breayanmt10@hotmail.com>
2023-03-17 17:45:54 -0600

    Spring-boot

---
+++ practica03/src/main/java/mx/uv/practica03/Configuracion.java
@@ -0,0 +1,18 @@
+package mx.uv.practica03;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ws.config.annotation.EnableWs;
+
+@EnableWs
+@Configuration
+public class Configuracion {
+    public void SaludosSchema(){
+
+    }
+    public void messageDispatchesServlet(){
+
+    }
+    public void defaultWsdDefinition(){
+        
+    }
+}
